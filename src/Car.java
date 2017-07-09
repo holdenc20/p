d1b0e47c03aba707a0fa808956a9bb05aa6e4f35
holdenc20 <league@WTS-IM-17.attlocal.net>
2017-07-09 11:32:07 -0700

    more
    ….

---
+++ src/Car.java
@@ -0,0 +1,26 @@
+
+public class Car {
+	int CarY;
+	int CarX;
+
+	public Car(int CarX, int CarY) {
+		this.CarY = CarY;
+		this.CarX = CarX;
+	}
+
+	public void setCarY(int CarY) {
+		this.CarY = CarY;
+	}
+
+	public void setCarX(int CarX) {
+		this.CarX = CarX;
+	}
+
+	public int getCarY() {
+		return CarY;
+	}
+
+	public int getCarX() {
+		return CarX;
+	}
+}
--- src/Frogger.java
+++ src/Frogger.java
@@ -3,66 +3,110 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Frogger extends JPanel implements KeyListener{
-	Font font=new Font(Font.SANS_SERIF, Font.BOLD,20);
+public class Frogger extends JPanel implements KeyListener {
+	Font font = new Font(Font.SANS_SERIF, Font.BOLD, 20);
 	Graphics g;
-	int gameState=0;//0 start 1 game 2 game over
-	JFrame p=new JFrame();
-	public Frogger(){
-		setup();
-		
-	}
-	public void paintComponent(Graphics G){
-		//super.paintComponent(G);
-		if(gameState==0){
-			System.out.println("0");
-			G.setColor(Color.GREEN);
-			G.fillRect(0, 0, 800,1600);
-			G.setColor(Color.BLACK);
-			G.drawString("Frogger", 100, 100);
-			G.setFont(font);
-			G.drawString("press space to start",100,200);
-		}
-		if(gameState==1){
-			System.out.println("1");
-			G.setColor(Color.GRAY);
-			G.fillRect(0, 0, 800, 1600);
-		}
-	}
-	
+	int counter = 0;
+	int gameState = 0;// 0 start 1 game 2 game over
+	JFrame p = new JFrame();
+	Random r = new Random();
+	int FrogSize = 50;
+	int FrogX = 400;
+	int FrogY = 900;
+	boolean isAlive = true;
+	ArrayList<Car> Cars = new ArrayList<Car>();
+	boolean gameStart = false;
 
-	void setup(){
-	 p.setVisible(true);
-	 p.add(this);
-	 p.setSize(800,1600);
-	 p.addKeyListener(this);
+	public Frogger() {
+		setup();
+
+		for (int p = 0; p < 15; p++) {
+			Car newCar = new Car(0, 50 * p);
+			Cars.add(newCar);
+		}
+
 	}
+
+	public void paintComponent(Graphics G) {
+		// super.paintComponent(G);
+		if (gameState == 0) {
+			G.setColor(Color.GREEN);
+			G.fillRect(0, 0, 800, 1000);
+			G.setColor(Color.BLACK);
+			G.setFont(font);
+			G.drawString("Frogger", 100, 100);
+			G.drawString("press space to start", 100, 200);
+		}
+		if (gameState == 1) {
+			counter++;
+			G.setColor(Color.GRAY);
+			G.fillRect(0, 0, 800, 1000);
+			G.setColor(Color.GREEN);
+			G.fillRect(FrogX, FrogY, FrogSize, FrogSize);
+			for (int x = 0; x < Cars.size(); x++) {
+				G.setColor(Color.BLACK);
+				G.fillRect(Cars.get(x).getCarX(), Cars.get(x).getCarY(), 50, 30);
+			}
+			if (counter % 1000 == 0) {
+				for (int v = 0; v < Cars.size(); v++) {
+					Cars.get(v).setCarX(Cars.get(v).getCarX() + 50);
+				}
+			}
+			repaint();
+		}
+	}
+
+	void setup() {
+		p.setVisible(true);
+		p.add(this);
+		p.setSize(800, 1000);
+		p.addKeyListener(this);
+	}
+
 	@Override
 	public void keyTyped(KeyEvent e) {
 
 	}
+
 	@Override
 	public void keyPressed(KeyEvent e) {
 
 	}
+
 	@Override
 	public void keyReleased(KeyEvent e) {
-		if(gameState==0){
-			if(e.getKeyCode()==32){
-				gameState=1;
-				System.out.println(gameState);
-		}
-			
-		}
-		else if(gameState==1){
-			
-		}
-		else if(gameState==2){
-			
+		if (gameState == 0) {
+			if (e.getKeyCode() == 32) {
+				gameState = 1;
+				gameStart = true;
+				repaint();
+			}
+
+		} else if (gameState == 1) {
+			if (e.getKeyCode() == 87) {
+				FrogY = FrogY - 50;
+			} else if (e.getKeyCode() == 65) {
+				if (FrogX > 0) {
+					FrogX = FrogX - 50;
+				}
+			} else if (e.getKeyCode() == 83) {
+				if (FrogY < 950) {
+					FrogY = FrogY + 50;
+				}
+			} else if (e.getKeyCode() == 68) {
+				if (FrogX < 750) {
+					FrogX = FrogX + 50;
+				}
+			}
+			repaint();
+		} else if (gameState == 2) {
+
 		}
 	}
 }
